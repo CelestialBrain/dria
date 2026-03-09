3fd7a6933af2bcaeaecef58fe2f3628e3d0fc925
Prince Wagan <princewagan08@gmail.com>
2026-03-09 23:45:35 +0800

    Initial Commit

---
+++ aird/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  aird
+//
+//  Created by Prince Wagan on 3/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ aird/airdApp.swift
@@ -0,0 +1,17 @@
+//
+//  airdApp.swift
+//  aird
+//
+//  Created by Prince Wagan on 3/9/26.
+//
+
+import SwiftUI
+
+@main
+struct airdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
